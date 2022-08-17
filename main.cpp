19c9bd9fb08566ea76d4dc820a02a668d755e3a1
Darijan Sekerov <darijan2002@gmail.com>
2022-08-17 15:24:23 +0200

    bafasdf

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <list>
+#include <vector>
 
 using namespace std;
 
@@ -38,6 +40,7 @@ struct Cat : Cell {
     }
 
 };
+
 int Cat::CAT_INSTANCES = 0;
 
 struct Mouse : Cell {
@@ -53,14 +56,15 @@ struct Mouse : Cell {
         jpos = j;
     }
 };
+
 int Mouse::MOUSE_INSTANCES = 0;
 
 default_random_engine rng(chrono::system_clock::now().time_since_epoch().count());
 uniform_int_distribution<int> dist(0, N - 1);
 
-void putAnimalsOnRandomLocations(vector<Cat>& cats, vector<Mouse>& mice)
-{
-    CELL_TYPE field[N][N] {};
+// what if we use lists instead of vectors?
+void putAnimalsOnRandomLocations(vector<Cat> &cats, vector<Mouse> &mice) {
+    CELL_TYPE field[N][N]{};
 
     for (int i = 0; i < N_CATS; i++) {
         int ti, tj;
@@ -88,15 +92,45 @@ void putAnimalsOnRandomLocations(vector<Cat>& cats, vector<Mouse>& mice)
     }
 }
 
-void printField(vector<Cat> const& cats, vector<Mouse> const& mice)
-{
+void printField(vector<Cat> const &cats, vector<Mouse> const &mice) {
     vector<string> field(N, string(N, '.'));
-    for(auto& c : cats) field[c.ipos][c.jpos] = 'C';
-    for(auto& m : mice) field[m.ipos][m.jpos] = 'M';
+    for (auto &c: cats) field[c.ipos][c.jpos] = 'C';
+    for (auto &m: mice) field[m.ipos][m.jpos] = 'M';
 
-    for(auto& str : field) cout << str << endl;
+    for (auto &str: field) cout << str << endl;
 }
 
+int runIteration(vector<Cat> &cats, vector<Mouse> &mice, int limit = 600) {
+    int tick;
+    if(limit <= 0) limit = 2000; // no (reasonable) limit
+
+    for(tick = 0; !mice.empty() && tick < limit; tick++)
+    {
+        vector<vector<Mouse>::iterator> eatenMice;
+        // update cats
+        for(auto& cat : cats)
+        {
+            // get mice in cat's radius
+            // if multiple, then eat the furthest
+            // if none, go to the nearest
+
+            // animals move in all directions
+        }
+
+        // update mice
+        // 1. delete eaten mice
+        for(auto& mouse : eatenMice) mice.erase(mouse);
+
+        // 2. move the rest
+        for(auto& mouse : mice)
+        {
+
+        }
+    }
+
+    // -1 - DNF
+    return (tick == limit) ? -1 : limit;
+}
 
 int main() {
     // Cell* field[N][N];
@@ -105,7 +139,16 @@ int main() {
     vector<Mouse> mice;
     putAnimalsOnRandomLocations(cats, mice);
 
+    cout << "BEFORE: " << endl;
     printField(cats, mice);
+    cout << endl;
+
+    int timeToFinish = runIteration(cats, mice);
+    cout << timeToFinish << endl;
+
+    cout << "AFTER: " << endl;
+    printField(cats, mice);
+    cout << endl;
 
     return 0;
 }
